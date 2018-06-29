83978630416ca50bf225836fcc99586bcf171430
mabelleguizamon <tanialeguizamon@gmail.com>
2018-06-29 09:57:54 -0400

    se crearon los grupos, asi como las opciones para modificar o eliminar un grupo

--- Proyecto/appSgp/app/src/main/java/com/example/carlos/appsgp/EliminarGrupoActivity.java
+++ Proyecto/appSgp/app/src/main/java/com/example/carlos/appsgp/EliminarGrupoActivity.java
@@ -1,13 +1,45 @@
 package com.example.carlos.appsgp;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class EliminarGrupoActivity extends AppCompatActivity {
+
+    Button botonEliminarGrupo;
+    EditText editTextEliminarIdGrupo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vista_grupo_eliminar);
+
+        botonEliminarGrupo = (Button) findViewById(R.id.buttonEliminarGrupo);
+        editTextEliminarIdGrupo = (EditText) findViewById(R.id.editTextEliminarIdGrupo);
+        botonEliminarGrupo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eliminarGrupo();
+            }
+        });
+
+    }
+
+    @SuppressLint("WrongConstant")
+    private void eliminarGrupo() {
+
+        int resp;
+        String url = "http://"+ServicioActivity.ip+"/Sgpis2/webresources/spis2.entities.sprint/"+editTextEliminarIdGrupo.getText().toString();
+        resp = ServicioActivity.delete(this,url);
+        if (resp != 204){
+            Toast.makeText(this,"El grupo no existe, verifique ID ingresado", 5).show();
+            return;
+        }
+        Toast.makeText(this,"Se ha eliminado el grupo exitosamente", 5).show();
+
     }
 }
--- Proyecto/appSgp/app/src/main/java/com/example/carlos/appsgp/GruposActivity.java
+++ Proyecto/appSgp/app/src/main/java/com/example/carlos/appsgp/GruposActivity.java
@@ -1,13 +1,73 @@
 package com.example.carlos.appsgp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class GruposActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> adaptadorColeccionGrupo;
+    ListView listaGrupo;
+    ArrayList<String> coleccionGrupo = new ArrayList<String>();
+
+
+    Button buttonGrupoAgregar;
+    Button buttonGrupoEditar;
+    Button buttonGrupoEliminar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vista_grupos);
+
+        Intent intentGrupo = getIntent();
+        coleccionGrupo = intentGrupo.getStringArrayListExtra("Coleccion");
+        listaGrupo = (ListView) findViewById(R.id.listViewGrupo);
+        adaptadorColeccionGrupo = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, coleccionGrupo);
+        listaGrupo.setAdapter(adaptadorColeccionGrupo);
+        buttonGrupoAgregar = (Button) findViewById(R.id.buttonGrupoAgregar);
+        buttonGrupoEliminar = (Button) findViewById(R.id.buttonGrupoEliminar);
+        buttonGrupoEditar = (Button) findViewById(R.id.buttonGrupoEditar);
+
+        buttonGrupoEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editarGrupo();
+            }
+        });
+        buttonGrupoAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                agregarGrupo();
+            }
+        });
+        buttonGrupoEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eliminarGrupo();
+            }
+        });
     }
+
+    private void agregarGrupo() {
+        Intent intentT = new Intent(this, AgregarGruposActivity.class);
+        startActivity(intentT);
+    }
+
+    private void editarGrupo() {
+        Intent intentT = new Intent(this, EditarGruposActivity.class);
+        startActivity(intentT);
+    }
+
+    private void eliminarGrupo() {
+        Intent intentT = new Intent(this, EliminarGrupoActivity.class);
+        startActivity(intentT);
+    }
+
 }
